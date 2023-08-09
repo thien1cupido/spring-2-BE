a0e606e41a392ac931898a67b5a845c5707306fb
thien1cupido <124018889+thien1cupido@users.noreply.github.com>
2023-08-09 14:02:03 +0700

    first commit

--- trung_thien_technology/src/main/java/com/example/trung_thien_technology/model/Customers.java
+++ trung_thien_technology/src/main/java/com/example/trung_thien_technology/model/Customers.java
@@ -15,63 +15,25 @@ public class Customers {
     private String name;
     @Column(nullable = false)
     private String birthday;
-    @Column(nullable = false)
     private int gender;
     @Column(nullable = false,unique = true,length = 10)
     private String phoneNumber;
     @Column(nullable = false,unique = true,length = 50)
     private String email;
-    @Column(nullable = false)
     private String address;
-    @Column(nullable = false,unique = true)
-    private String citizenCode;
+
     @Column(nullable = false)
     private String image;
-    @Column(nullable = false)
-    private String frontCitizen;
-    @Column(nullable = false)
-    private String backCitizen;
-    private String quantityContract;
     @Column(name = "create_date", columnDefinition = "DATETIME DEFAULT now()", updatable = false)
     private LocalDateTime createDate;
     @Column(name = "update_date", columnDefinition = "DATETIME DEFAULT now()")
     @UpdateTimestamp
     private LocalDateTime updateDate;
-    @Column(columnDefinition = "BIT DEFAULT 0", updatable = true)
+    @Column(columnDefinition = "BIT DEFAULT 0")
     private boolean isDelete;
-    @Column(name = "note", columnDefinition = "text")
-    private String note;
-
-    public String getQuantityContract() {
-        return quantityContract;
-    }
-
-    public void setQuantityContract(String quantityContract) {
-        this.quantityContract = quantityContract;
-    }
-
 
     public Customers() {
     }
-
-    public Customers(Long id, String name, String birthday, int gender, String phoneNumber, String email, String address, String citizenCode, String image, String frondCitizen, String backCitizen, String quantityContract, LocalDateTime createDate, LocalDateTime updateDate, boolean isDelete) {
-        this.id = id;
-        this.name = name;
-        this.birthday = birthday;
-        this.gender = gender;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.address = address;
-        this.citizenCode = citizenCode;
-        this.image = image;
-        this.frontCitizen = frondCitizen;
-        this.backCitizen = backCitizen;
-        this.quantityContract = quantityContract;
-        this.createDate = createDate;
-        this.updateDate = updateDate;
-        this.isDelete = isDelete;
-    }
-
 
     public Long getId() {
         return id;
@@ -129,36 +91,12 @@ public class Customers {
         this.address = address;
     }
 
-    public String getCitizenCode() {
-        return citizenCode;
-    }
-
-    public void setCitizenCode(String citizenCode) {
-        this.citizenCode = citizenCode;
-    }
-
     public String getImage() {
         return image;
     }
 
     public void setImage(String image) {
         this.image = image;
-    }
-
-    public String getFrontCitizen() {
-        return frontCitizen;
-    }
-
-    public void setFrontCitizen(String frontCitizen) {
-        this.frontCitizen = frontCitizen;
-    }
-
-    public String getBackCitizen() {
-        return backCitizen;
-    }
-
-    public void setBackCitizen(String backCitizen) {
-        this.backCitizen = backCitizen;
     }
 
     public LocalDateTime getCreateDate() {
